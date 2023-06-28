0097033480db483bb69f26c396eef58a10b8bf08
Amanda Simonds <amandasimonds9@gmail.com>
2023-06-28 16:09:45 -0400

    moved arrays to their own files, added styling, added word count option

--- script.js
+++ script.js
@@ -1,99 +1,37 @@
-const adjectiveArray1 = [
-	'the',
-	'first',
-	'only',
-	'a',
-	'one',
-	'strange',
-	'concluded',
-	'upright',
-	'all_Seeing',
-	'arctic',
-	'active',
-	'i',
-	'am',
-	'1',
-	'sparkling',
-	'limitless',
-	'cosmic',
-    'doctor'
-];
-const adjectiveArray2 = [
-	'lone',
-	'last',
-	'mossy',
-	'fiery',
-	'disco',
-    'significant',
-	'sparkly',
-	'cat',
-	'fox',
-	'world',
-	'aura',
-	'beach',
-	'fuzzy',
-	'generic',
-	'and_a',
-	'fantastic',
-	'dark',
-	'cactus',
-	'lost',
-];
-const wordArray = [
-	'moon',
-	'planet',
-	'life',
-	'fireball',
-	'jupiter',
-	'stick',
-	'leaf',
-	'mushroom',
-	'bb',
-	'alien',
-	'hooman',
-	'comet',
-	'pokemon',
-	'hamster',
-	'wizard',
-	'sphinx',
-	'whale',
-	'legend',
-	'holiday',
-	'dragonfly',
-	'dancer',
-];
+import { adjectiveArray1 } from './word-arrays/adjective-array-1.js';
+import { adjectiveArray2 } from './word-arrays/adjective-array-2.js';
+import { wordArray } from './word-arrays/word-array.js';
 
-const spacerArray = ['', '-', '_'];
+const spacerArray = ['', '-', '_', '-', '_', '-', '_', '-', '_'];
 
-const generateUsername = () => {
-    if (wordNumber === 3) {
-
-        const firstWord =
-		adjectiveArray1[Math.floor(Math.random() * adjectiveArray1.length)];
-        const secondWord =
-		adjectiveArray2[Math.floor(Math.random() * adjectiveArray2.length)];
-        const thirdWord = wordArray[Math.floor(Math.random() * wordArray.length)];
-        const spacer1 = spacerArray[Math.floor(Math.random() * spacerArray.length)];
-        const spacer2 = spacerArray[Math.floor(Math.random() * spacerArray.length)];
-        
-        const generatedWord =
-		firstWord + spacer1 + secondWord + spacer2 + thirdWord;
-    } else {
-        const newArray = adjectiveArray1.concat(adjectiveArray2)
-        const firstWord =
-		adjectiveArray1[Math.floor(Math.random() * adjectiveArray1.length)];
-        const secondWord =
-		adjectiveArray2[Math.floor(Math.random() * adjectiveArray2.length)];
-        const thirdWord = wordArray[Math.floor(Math.random() * wordArray.length)];
-        const spacer1 = spacerArray[Math.floor(Math.random() * spacerArray.length)];
-        const spacer2 = spacerArray[Math.floor(Math.random() * spacerArray.length)];
-        
-        const generatedWord =
-		firstWord + spacer1 + secondWord + spacer2 + thirdWord;
-    }
-
-	document.getElementById('generatedWord').innerHTML = generatedWord;
-	console.log(wordArray[Math.floor(Math.random() * wordArray.length)]);
+const generateUsername = (event) => {
+	event.preventDefault();
+	const wordCount2 = document.getElementById('wordCount2').checked;
+	console.log(wordCount3);
+	if (wordCount2) {
+		const newArray = adjectiveArray1.concat(adjectiveArray2);
+		const firstWord = newArray[Math.floor(Math.random() * newArray.length)];
+		const thirdWord =
+			wordArray[Math.floor(Math.random() * wordArray.length)];
+		const spacer =
+			spacerArray[Math.floor(Math.random() * spacerArray.length)];
+		const generatedWord = firstWord + spacer + thirdWord;
+		document.getElementById('generatedWord').innerHTML = generatedWord;
+	} else {
+		const firstWord =
+			adjectiveArray1[Math.floor(Math.random() * adjectiveArray1.length)];
+		const secondWord =
+			adjectiveArray2[Math.floor(Math.random() * adjectiveArray2.length)];
+		const thirdWord =
+			wordArray[Math.floor(Math.random() * wordArray.length)];
+		const spacer1 =
+			spacerArray[Math.floor(Math.random() * spacerArray.length)];
+		const spacer2 =
+			spacerArray[Math.floor(Math.random() * spacerArray.length)];
+		const generatedWord =
+			firstWord + spacer1 + secondWord + spacer2 + thirdWord;
+		document.getElementById('generatedWord').innerHTML = generatedWord;
+	}
 };
 
 document
